a0d5357976faa610216f18067ba1de357abb7a3f
Nicolás Bergerman <nico.ber2005@gmail.com>
2026-03-15 11:29:31 -0300

    Copia Inicial de Archivos

---
+++ app/src/main/java/com/example/puntaje_buraco_3/ui/AgregarFragment.java
@@ -0,0 +1,31 @@
+package com.example.puntaje_buraco_3.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.puntaje_buraco_3.R;
+
+public class AgregarFragment extends Fragment {
+
+    public AgregarFragment() {
+        // Constructor vacío requerido
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_agregar, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/example/puntaje_buraco_3/ui/HistorialFragment.java
@@ -0,0 +1,31 @@
+package com.example.puntaje_buraco_3.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.puntaje_buraco_3.R;
+
+public class HistorialFragment extends Fragment {
+
+    public HistorialFragment() {
+        // Constructor vacío requerido
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_historial, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
